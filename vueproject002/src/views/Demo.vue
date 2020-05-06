808ccad8c62cd02644dbccd2e29cbe6b658fa7b1
petershowx <xiaoyulong1988@gmail.com>
2020-05-06 16:49:24 +0800

    vue.js教程\chapter 01 Installation.html vue.js教程\chapter 02 Creating a Vue Instance.html vue.js教程\chapter 03 Data and Methods.html vue.js教程\chapter 04 Instance Lifecycle Hooks.html vue.js教程\chapter 05 Template Syntax - Interpolations.html vue.js教程\chapter 06 Template Syntax - Shorthands.html vue.js教程\chapter 07 Class and Style Bindings.html vue.js教程\chapter 08 Conditional Rendering.html vue.js教程\chapter 09 List Rendering.html vue.js教程\chapter 10 Event Handling.html vue.js教程\chapter 11 Form Input Bindings.html vue.js教程\chapter 12 Components Basics.html vue.js教程\chapter 13 Components Registration.html vue.js教程\chapter 14 Single File Components.md vue.js教程\vue.js vueproject001\.browserslistrc vueproject001\.editorconfig vueproject001\.eslintrc.js vueproject001\.gitignore vueproject001\README.md vueproject001\babel.config.js vueproject001\package.json vueproject001\public\favicon.ico vueproject001\public\index.html vueproject001\src\App.vue vueproject001\src\assets\logo.png vueproject001\src\components\HelloWorld.vue vueproject001\src\main.ts vueproject001\src\router\index.ts vueproject001\src\shims-tsx.d.ts vueproject001\src\shims-vue.d.ts vueproject001\src\store\index.ts vueproject001\src\views\About.vue vueproject001\src\views\Home.vue vueproject001\tsconfig.json vueproject001\yarn.lock vueproject002\.browserslistrc vueproject002\.editorconfig vueproject002\.eslintrc.js vueproject002\.gitignore vueproject002\README.md vueproject002\babel.config.js vueproject002\package.json vueproject002\public\favicon.ico vueproject002\public\index.html vueproject002\src\App.vue vueproject002\src\assets\logo.png vueproject002\src\components\HelloWorld.vue vueproject002\src\main.js vueproject002\src\router\index.js vueproject002\src\store\index.js vueproject002\src\views\About.vue vueproject002\src\views\Demo.vue vueproject002\src\views\Home.vue vueproject002\src\views\Info.vue vueproject002\yarn.lock

---
+++ vueproject002/src/views/Demo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    hello info component
+    <div>
+      <ul>
+       <li v-for="(item,index) in Lists"
+           @click="choose(index)"
+           :class="{active: index == current && current != ''}"
+           :key="index">
+        {{item}}
+       </li>
+        </ul>
+        <button type="button" @click="mdd()">添加</button>
+        <ul>
+          <li v-for="(item,index) in target" :key="index">{{item}}</li>
+        </ul>
+    </div>
+
+     <button type="button"  v-on:click="add()">转移</button>
+  </div>
+</template>
+<script>
+import Store from '@/store';
+
+export default {
+  name: 'Demo',
+  Store,
+  data() {
+    return {
+      current: '',
+      Lists: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      target: [],
+    };
+  },
+  mounted() {
+    window.Vue = this;
+  },
+  methods: {
+    add() {
+      // debugger;
+      console.log('add event');
+      Store.commit('increase');
+    },
+    mdd() {
+      // debugger;
+      if (this.current === '') return;
+      console.log('add event');
+      this.$data.target.push(this.$data.Lists[this.current]);
+      this.current = '';
+    },
+    choose(index) {
+      console.log(index);
+      this.$data.current = index;
+    },
+
+  },
+};
+</script>
+
+<style scoped>
+  li.active{
+    background-color: green;
+  }
+</style>
---
+++ vueproject002/src/views/Info.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    hello info component
+     <button type="button"  v-on:click="add()">添加</button>
+  </div>
+</template>
+<script>
+import Store from '@/store';
+
+export default {
+  name: 'Info',
+  Store,
+  mounted() {
+    window.Vue = this;
+  },
+  methods: {
+    add() {
+      // debugger;
+      console.log('add event');
+      Store.commit('increase');
+    },
+
+  },
+};
+</script>
+
+<style scoped>
+</style>
